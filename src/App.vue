5b37ec6bed115d83a10abc33734476677a652540
jackeryjam <jackeryjam@gmail.com>
2017-12-24 20:55:10 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,6 @@ export default class App extends Vue {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 30px;
+  margin-top: 40px;
 }
 </style>
